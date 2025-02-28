97c856d9fc5d1fb884fe7ceea77662b0204a6d92
SURBHI SINHA <139940072+surbhisinha12@users.noreply.github.com>
2025-02-28 09:00:25 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,9 @@ const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(cors({
+  origin: 'https://mern-app-new-frontend.vercel.app'
+}));
 app.use('/auth',  AuthRouter);
 app.use('/products' ,ProductRouter);
 
